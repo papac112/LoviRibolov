4c6c5f729619b9c7aa3fd4895241293711e87e92
papac112 <134517865+papac112@users.noreply.github.com>
2023-06-07 14:44:49 +0200

    Update header.h

--- header.h
+++ header.h
@@ -17,3 +17,42 @@ int ispis_hrane(struct hrana*);
 int ispis_puski(struct puske*);
 int ispis_pribora(struct pribor*);
 int ispis_municije(struct municija *);
+
+
+
+
+
+struct stap {
+	float cijena_stap;
+	char naziv_stap[20];
+	int kolicina_stap;
+};
+struct hrana {
+	float cijena_hrana;
+	char naziv_hrana[20];
+	int kolicina_hrana;
+};
+
+
+struct puske {
+	float cijena_puske;
+	char naziv_puske[20];
+	int kolicina_puske;
+};
+
+struct pribor {
+	float cijena_pribor;
+	char naziv_pribor[20];
+	int kolicina_pribor;
+};
+
+
+struct municija {
+	float cijena_municija;
+	char naziv_municija[20];
+	int kolicina_municija;
+};
+
+
+
+#endif
